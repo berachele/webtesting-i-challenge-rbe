3d616269f140ec6dc71043a8cc529d5b45b3f203
Rachele Edwards <dot.com1472@gmail.com>
2020-05-20 13:32:08 -0600

    installed jest and wrote skeleton for testing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,23 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+
+describe('enhancer', () => {
+    describe('repair', () => {
+        it('should return a new item with durability restored to 100', () => {
+    
+        })
+    })
+
+    describe('succeed', () => {
+        it('Enhancement should increase by 1', () => {})
+        it('Enhancement should be unchanged if level is already at 20', () => {})
+        it('Durability should not be changed', () => {})
+    })
+
+    describe('fail', () => {
+        it('Durability should decrease by 5 if Enhancement is less than 15', () => {})
+        it('Durability should decrease by 10 if Enhancement is greater than 15', () => {})
+        it('Enhancement should decrease by 1 if Enhancement is greater than 16', () => {})
+    })
+})
