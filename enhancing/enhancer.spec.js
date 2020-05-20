f5792052270f9421ecfab04e25b067f18a6f4d4f
Rachele Edwards <dot.com1472@gmail.com>
2020-05-20 13:33:44 -0600

    made todo's to tests that I havent gotten to yet

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -10,14 +10,14 @@ describe('enhancer', () => {
     })
 
     describe('succeed', () => {
-        it('Enhancement should increase by 1', () => {})
-        it('Enhancement should be unchanged if level is already at 20', () => {})
-        it('Durability should not be changed', () => {})
+        it.todo('Enhancement should increase by 1', () => {})
+        it.todo('Enhancement should be unchanged if level is already at 20', () => {})
+        it.todo('Durability should not be changed', () => {})
     })
 
     describe('fail', () => {
-        it('Durability should decrease by 5 if Enhancement is less than 15', () => {})
-        it('Durability should decrease by 10 if Enhancement is greater than 15', () => {})
-        it('Enhancement should decrease by 1 if Enhancement is greater than 16', () => {})
+        it.todo('Durability should decrease by 5 if Enhancement is less than 15', () => {})
+        it.todo('Durability should decrease by 10 if Enhancement is greater than 15', () => {})
+        it.todo('Enhancement should decrease by 1 if Enhancement is greater than 16', () => {})
     })
 })
